ac46ba6a5e96a9561d4846be42450ed6524584d8
supreme-hub <shoryaagarwal831@gmail.com>
2023-01-19 21:10:37 +0530

    added two player func

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const HB = document.querySelector('#Human')
 let GameType = 1;
 let TTTBoard = [[0,0,0],[0,0,0],[0,0,0]];
 let Players= [];
+// 0 = false, 1 = O, 2 = 1, 3 = Tie 
+let WinStat = 0;
 
 
 
@@ -65,10 +67,94 @@ function CreateCell(a,b) {
             warnBox.innerHTML = "Another Player has used this spot"
             warnBox.style.visibility = 'visible'
         }
+        CheckWin()
+        if (WinStat === 1) {
+            board.style.display = 'none'
+            warnBox.innerHTML = "Player 1 has won"
+            warnBox.style.visibility = 'visible'
+        }
+        if (WinStat === 2) {
+            board.style.display = 'none'
+            warnBox.innerHTML = "Player 2 has won"
+            warnBox.style.visibility = 'visible'
+        }
+        if (WinStat === 3) {
+            board.style.display = 'none'
+            warnBox.innerHTML = "It is a Tie"
+            warnBox.style.visibility = 'visible'
+        }
     })
     board.appendChild(cell);
 }
 
+function CheckWin() {
+    if (LineCheck(1) === true || ColumnCheck(1) === true || DiaCheck(1) === true) {
+        WinStat = 1
+    }
+    if (LineCheck(2) === true || ColumnCheck(2) === true || DiaCheck(2) === true) {
+        WinStat = 2
+    }
+    if (TieCheck() === true) {
+        WinStat = 3
+    }
+}
+
+function TieCheck() {
+    let stat = 0;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++){
+            if (TTTBoard[i][j] !== 0) {
+                stat++
+                if (stat === 9) {
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+
+function LineCheck(a) {
+    let stat = 0;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++){
+            if (TTTBoard[i][j] === a) {
+                stat++
+                if (stat === 3) {
+                    return true
+                }
+            }
+        }
+        stat = 0;
+    }
+    return false
+} 
+
+function ColumnCheck(a) {
+    if (TTTBoard[0][0] === a &&
+        TTTBoard[1][0] === a &&
+        TTTBoard[2][0] === a)
+        return true
+    if (TTTBoard[0][1] === a &&
+        TTTBoard[1][1] === a &&
+        TTTBoard[2][1] === a)
+        return true
+    if (TTTBoard[0][2] === a &&
+        TTTBoard[1][2] === a &&
+        TTTBoard[2][2] === a)
+        return true
+    return false
+}
+
+function DiaCheck(a) {
+    if (TTTBoard[0][0] === a && TTTBoard[1][1] === a && TTTBoard[2][2] === a) {
+        return true
+    }
+    if (TTTBoard[0][2] === a && TTTBoard[1][1] === a && TTTBoard[2][0] === a) {
+        return true
+    }
+    return false
+}
 
 CB.addEventListener('click', () => {
     CB.style.color = "rgb(190, 186, 186)"
@@ -108,4 +194,7 @@ function GameStart() {
         Pf = PlayerM(2, 1)
         Players.push(Pf)
     }
+    warnBox.style.visibility = 'collapse'
+    board.style.display = 'grid'
+    WinStat = 0
 }
